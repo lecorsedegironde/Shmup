6d950ea301afb6cfba97499b68dc2ad5e723618f
bhoffstetter <bastien.hoffstetter@etu.u-bordeaux.fr>
2015-04-15 18:00:05 +0200

    Esthetic changes

--- MovableElement.h
+++ MovableElement.h
@@ -4,9 +4,21 @@
 
 class MovableElement
 {
+
+protected:
+    int m_x;
+    int m_y;
+    int m_w;
+    int m_h;
+    int m_dx;
+    int m_dy;
+    bool m_etat;
+
 public:
     MovableElement();
     MovableElement(int x, int y, int w, int h, int dx, int dy);
+    virtual ~MovableElement();
+
     int getX() const;
     int getY() const;
     int getW() const;
@@ -23,17 +35,7 @@ public:
     void setDy(const int &dy);
     void setEtat(const bool &etat);
 
-    virtual ~MovableElement();
     virtual void detruire() {}
-
-protected:
-    int m_x;
-    int m_y;
-    int m_w;
-    int m_h;
-    int m_dx;
-    int m_dy;
-    bool m_etat;
 };
 
 #endif // MOVABLE_ELEMENT_H
